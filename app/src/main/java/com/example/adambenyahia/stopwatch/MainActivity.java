04879cd966e2fce43e0ce27db0badf196cf359c2
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-09-25 01:06:04 +0200

    moving on

--- app/src/main/java/com/example/adambenyahia/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/adambenyahia/stopwatch/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.adambenyahia.stopwatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int seconds=0;
+    private boolean running;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStart(View view){
+        running=true;
+
+    }
+
+    public void onClickStop(View view){
+        running=false;
+
+    }
+
+    public void onClickReset(View view){
+        running=false;
+        seconds=0;
+
     }
 }
