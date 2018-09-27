51a43fbc12241055e6829df620b0490bd25dec6b
FullFrontal92 <adam.benyahia@hotmail.fr>
2018-09-27 18:48:37 +0200

    moving on

--- app/src/main/java/com/example/adambenyahia/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/adambenyahia/stopwatch/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.adambenyahia.stopwatch;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        runtimer();
     }
 
     public void onClickStart(View view){
@@ -27,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
     public void onClickReset(View view){
         running=false;
         seconds=0;
+
+    }
+
+    private void runtimer(){
+
+        final TextView timeview= findViewById(R.id.time_view);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours= seconds/3600;
+                int minutes= (seconds%3600)/60;
+                int secs= seconds%60;
+
+                String time= String.format("%02d:%02d:%02d",hours,minutes,secs);
+
+                timeview.setText(time);
+
+                if (running){
+
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
 
     }
 }
